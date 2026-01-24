661e7f9baa236c7a2a881ada2e288d4fbfa463fe
kebbicheanes <kebbicheanes@gmail.com>
2026-01-24 18:15:38 +0100

    usermanage header

--- Headers/usermanage_3.h
+++ Headers/usermanage_3.h
@@ -6,5 +6,26 @@ struct User {
     int role; // 0: user, 1: admin
     int state; // 0: active, 1: blocked
 };
+int comparechars(char a[], char b[])
+void initUsers(struct User users[], int n)
+void displayUsers(struct User users[], int n)
+void addUser(struct User users[], int n)
+void deleteuser(struct User users[], int n, char name[])
+int searchUser(struct User users[], int n, char name[])
+void changepassword(struct User users[], int n, char name [])
+int checklogin(struct User users[], int n, char name[], char pass[])
+int strongpassword(char pass[])
+void blockuser(struct User users[], int n, char name[])
+void unblockuser(struct User users[], int n, char name[])
+void changerole(struct User users[], int n, char name[], int role)
+void listadmins(struct User users[], int n)
+int stringlength(char str[])
+int containsuppercase(char str[])
+int containslowercase(char str[])
+int containsdigit(char str[])
+int containspecialsymbol(char str[])
+void userstatistics(struct User users[], int n)
+void saveUsers(struct User users[], int n)
+void loadUsers(struct User users[], int n)
 
 #endif 
