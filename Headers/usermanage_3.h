43b7a42af5c9158b1e580600a196e92d95a546a8
kebbicheanes <kebbicheanes@gmail.com>
2026-01-24 18:12:24 +0100

    usermanage header

--- Headers/usermanage_3.h
+++ Headers/usermanage_3.h
@@ -1,6 +1,10 @@
 #ifndef USERMANAGE_4_H
 #define USERMANAGE_3_H
-
-/* declarations here */
+struct User {
+    char name[20];
+    char password[20];
+    int role; // 0: user, 1: admin
+    int state; // 0: active, 1: blocked
+};
 
 #endif 
